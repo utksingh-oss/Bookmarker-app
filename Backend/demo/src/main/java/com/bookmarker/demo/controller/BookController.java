620c069718bb2e8e0b840039f7868e6c8303291a
utkarsh singh <utk608@gmail.com>
2022-10-06 12:19:00 +0530

    fixed CORS issue

--- Backend/demo/src/main/java/com/bookmarker/demo/controller/BookController.java
+++ Backend/demo/src/main/java/com/bookmarker/demo/controller/BookController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/books")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class BookController{
     private final BookService bookService;
 
